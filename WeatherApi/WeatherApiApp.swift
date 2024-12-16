b494e22f76d09ba13e11486994acb54516ae50b2
Elena Rubilova <yelena.rubilova@gmail.com>
2024-12-16 14:39:14 -0500

    Initial Commit

---
+++ WeatherApi/WeatherApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApiApp.swift
+//  WeatherApi
+//
+//  Created by Elena Rubilova on 12/16/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeatherApiTests/WeatherApiTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApiTests.swift
+//  WeatherApiTests
+//
+//  Created by Elena Rubilova on 12/16/24.
+//
+
+import Testing
+@testable import WeatherApi
+
+struct WeatherApiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
