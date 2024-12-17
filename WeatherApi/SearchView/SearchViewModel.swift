1a016cd0cbcf63de33c24720b4135689bd7c08f4
Elena Rubilova <yelena.rubilova@gmail.com>
2024-12-17 11:13:16 -0500

    Update SearchViewModel to use WeatherServiceProtocol

--- WeatherApi/SearchView/SearchViewModel.swift
+++ WeatherApi/SearchView/SearchViewModel.swift
@@ -10,7 +10,7 @@ import Combine
 
 class SearchViewModel: ObservableObject {
     
-    @Published var weather: Weather?
+    private let weatherService: WeatherServiceProtocol
     @Published var cities: [City] = []
     @Published var searchText: String = ""
     @Published var error = false
@@ -20,22 +20,53 @@ class SearchViewModel: ObservableObject {
     
     
     
-    init() {
+    init(_ weatherService: WeatherServiceProtocol) {
+        self.weatherService = weatherService
         addSubscribers()
     }
     
     func addSubscribers() {
-        $searchText.sink{ [weak self] (searchString) in
-            guard !searchString.isEmpty else { return }
-            print(searchString)
-            self?.dataService.getWeather(city: searchString)
-        }
-        .store(in: &cancellables)
-        
-        dataService.$weather
-            .sink{[weak self] (returnedWeather) in
-                self?.weather = returnedWeather
+        $searchText
+            .debounce(for: .milliseconds(300), scheduler: DispatchQueue.main)
+            .map { [weak self] searchText -> AnyPublisher<[City], Error> in
+                guard let self = self else { return Fail(error: RemoteError.connectivity).eraseToAnyPublisher() }
+                return self.weatherService.searchCities(searchText: searchText)
+            }
+            .switchToLatest()
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] completion in
+                switch completion {
+                case .finished:
+                    self?.error = false
+                case .failure:
+                    self?.error = true
+                }
+            } receiveValue: { [weak self] cities in
+                self?.cities = cities
             }
             .store(in: &cancellables)
     }
+    
+    /*private func addSubscribers() {
+        $searchText
+            .debounce(for: .milliseconds(300), scheduler: DispatchQueue.main)
+            .map { [weak self] searchText -> AnyPublisher<[City], Error> in
+                guard let self = self else { return Fail(error: NetworkError.somethingWentWrong).eraseToAnyPublisher() }
+                return self.networkManager.searchCities(searchText: searchText)
+            }
+            .switchToLatest()
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] completion in
+                switch completion {
+                case .finished:
+                    self?.error = false
+                case .failure:
+                    self?.error = true
+                }
+            } receiveValue: { [weak self] cities in
+                self?.cities = cities
+            }
+            .store(in: &cancellables)
+        self.searchText = searchTextStorage
+    }*/
 }
--- WeatherApi/WeatherService.swift
+++ WeatherApi/WeatherService.swift
@@ -8,26 +8,76 @@
 import Foundation
 import Combine
 
-class WeatherService {
-    
-    @Published var weather: Weather?
+protocol WeatherServiceProtocol {
+    func searchCities(searchText: String) -> AnyPublisher<[City], Error>
+}
+
+class WeatherService: WeatherServiceProtocol {
     
     var weatherSubscription: AnyCancellable?
     private let apiKey = "b38026405298410697632906241612"
     
-    /*init() {
-     getWeather()
-     }*/
+    private enum Method: String {
+        case search, current
+    }
     
-    func getWeather(city: String) {
-        guard let url = URL(string: "http://api.weatherapi.com/v1/current.json?key=\(apiKey)&q=\(city)&aqi=no") else { return }
-        
-        weatherSubscription = NetworkingManager.download(url: url)
-            .decode(type: Weather.self, decoder: JSONDecoder())
-            .receive(on: DispatchQueue.main)
-            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] (returnedWeather) in
-                self?.weather = returnedWeather
-                self?.weatherSubscription?.cancel()
-            })
+    func searchCities(searchText: String) -> AnyPublisher<[City], Error> {
+        guard !searchText.isEmpty else {
+            return Just([City]())
+                .setFailureType(to: Error.self)
+                .eraseToAnyPublisher()
+        }
+        guard let url = makeUrl(method: .search, query: searchText) else {
+            return Fail(error: RemoteError.connectivity).eraseToAnyPublisher()
+        }
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .map(\.data)
+            .decode(type: [CitySearchResponse].self, decoder: JSONDecoder())
+            .map { self.fetch(citiesIDs: $0.map(\.id)) }
+            .switchToLatest()
+            .catch { error in
+                return Fail(error: error).eraseToAnyPublisher()
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    private func fetch(citiesIDs: [Int]) -> AnyPublisher<[City], Error> {
+        guard !citiesIDs.isEmpty else {
+            return Just([City]())
+                .setFailureType(to: Error.self)
+                .eraseToAnyPublisher()
+        }
+        guard let url = makeUrl(method: .current, query: "bulk") else {
+            return Fail(error: RemoteError.connectivity).eraseToAnyPublisher()
+        }
+        let ids = citiesIDs.map { "id:\($0)"}
+        let payload = ["locations": ids.map { ["q": $0] }]
+        guard let jsonData = try? JSONSerialization.data(withJSONObject: payload) else {
+            return  Fail(error: RemoteError.invalidData).eraseToAnyPublisher()
+        }
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = jsonData
+        return URLSession.shared.dataTaskPublisher(for: request)
+            .map(\.data)
+            .decode(type: CitiesResponse.self, decoder: JSONDecoder())
+            .map { response in
+                response.bulk.map { $0.query.toCity() }
+            }
+            .catch { error in
+                return Fail(error: error).eraseToAnyPublisher()
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    private func makeUrl(method: Method, query: String) -> URL? {
+        URL(string: "https://api.weatherapi.com/v1/\(method.rawValue).json?key=\(apiKey)&q=\(query)")
     }
 }
+
+enum RemoteError: Swift.Error {
+    case connectivity
+    case invalidData
+}
+
